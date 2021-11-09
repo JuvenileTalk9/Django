f02ad6dd9b19a5c06050cdcbef9adf498606d705
JuvenileTalk9 <prdmq578@gmail.com>
2021-11-09 11:58:06 +0900

    sampleproject

--- sampleproject/blog/views.py
+++ sampleproject/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from .models import BlogPost
 
-
-class IndexView(TemplateView):
+class IndexView(ListView):
     
     template_name = 'index.html'
+    context_object_name = 'orderby_records'
+    queryset = BlogPost.objects.order_by('-posted_at')
