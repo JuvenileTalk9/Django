20d847ab31444be8ce0714dab9b91c918b20696e
JuvenileTalk9 <prdmq578@gmail.com>
2021-11-10 02:17:28 +0900

    sampleproject

--- sampleproject/blog/views.py
+++ sampleproject/blog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 from .models import BlogPost
 
 class IndexView(ListView):
     
     template_name = 'index.html'
-    context_object_name = 'orderby_records'
-    queryset = BlogPost.objects.order_by('-posted_at')
+    model = BlogPost
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['hello'] = 'Hello World!'
+        return context
+        
