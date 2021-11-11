1b30c23f98b70aa477367adf6eebcc20f42e1e95
JuvenileTalk9 <prdmq578@gmail.com>
2021-11-11 15:00:21 +0900

    sampleproject

--- sampleproject/blog/views.py
+++ sampleproject/blog/views.py
@@ -1,13 +1,23 @@
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 from .models import BlogPost
+from .forms import BlogPostForm
 
-class IndexView(ListView):
+class IndexListView(ListView):
     
     template_name = 'index.html'
     model = BlogPost
+    paginate_by = 1
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['hello'] = 'Hello World!'
         return context
-        
+
+
+class IndexView(CreateView):
+    
+    template_name = 'index.html'
+    form_class = BlogPostForm
+    success_url = "/"
+    
